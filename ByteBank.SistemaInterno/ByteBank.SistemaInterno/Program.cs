3e86beb115acb3387f9252932861dea5e6a3a755
Matheus Nicolas <manijrprof@gmail.com>
2022-02-26 20:51:04 -0300

    Curso parte5

---
+++ ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ByteBank.Modelos;
+using Humanizer;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime datafimPagamento = new DateTime(2022, 2, 18);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = TimeSpan.FromMinutes(120);//datafimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(8484, 46548);
+
+            new ContaCorrente(123456, 1651651);
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
